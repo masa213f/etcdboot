e138e2ac56cbcffe9e50b29dfac3136b5e90d99f
Masayuki Ishii <masa213f@gmail.com>
2021-04-11 15:38:46 +0000

    Add systemd package
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- cmd/etcdboot/main.go
+++ cmd/etcdboot/main.go
@@ -1,47 +1,13 @@
 package main
 
 import (
-	"bytes"
 	_ "embed"
 	"fmt"
 	"os"
-	"path/filepath"
-	"text/template"
 
 	"github.com/masa213f/etcdboot/pkg/config"
+	"github.com/masa213f/etcdboot/pkg/systemd"
 )
-
-//go:embed template/etcd.service
-var etcdServiceTemplateRaw string
-var etcdServiceTemplate = template.Must(template.New("").Parse(etcdServiceTemplateRaw))
-
-type etcdServiceParams struct {
-	MemberName          string
-	DataDir             string
-	ListenClientURL     string
-	ListenPeerURL       string
-	InitialCluster      string
-	InitialClusterToken string
-}
-
-func generateServiceFile(filename, memberName string, cluster *config.EtcdCluster) error {
-	param := &etcdServiceParams{
-		MemberName:          memberName,
-		DataDir:             filepath.Join("/var/lib", filename),
-		ListenClientURL:     cluster.GetMember(memberName).ClientURL,
-		ListenPeerURL:       cluster.GetMember(memberName).PeerURL,
-		InitialCluster:      cluster.InitialCluster(),
-		InitialClusterToken: cluster.Name + "-token",
-	}
-
-	buf := new(bytes.Buffer)
-	err := etcdServiceTemplate.Execute(buf, param)
-	if err != nil {
-		return err
-	}
-	fullpath := filepath.Join("/etc/systemd/system/", filename+".service")
-	return os.WriteFile(fullpath, buf.Bytes(), 0644)
-}
 
 func main() {
 	args := os.Args[1:]
@@ -56,7 +22,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = generateServiceFile(args[0], args[1], cluster)
+	err = systemd.CreateServiceFile(args[0], args[1], cluster)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
